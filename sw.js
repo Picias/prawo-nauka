787d353d2973c8e643b7804f86cf41d456e0da4d
Picias <165202738+Picias@users.noreply.github.com>
2026-03-13 22:03:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nauka-v2';
+const CACHE_NAME = 'nauka-v3';
 const ZASOBY_DO_ZAPISANIA = [
     './',
     './index.html',
@@ -8,24 +8,33 @@ const ZASOBY_DO_ZAPISANIA = [
     './ikona.png'
 ];
 
-// 1. Krok instalacji: pobieranie plików do pamięci telefonu
+// 1. Krok instalacji: pobieranie plików
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Zapisywanie plików w pamięci podręcznej (cache)');
-                return cache.addAll(ZASOBY_DO_ZAPISANIA);
-            })
+            .then((cache) => cache.addAll(ZASOBY_DO_ZAPISANIA))
     );
 });
 
-// 2. Krok obsługi zapytań: serwowanie plików z pamięci (działanie offline)
+// 2. NOWOŚĆ: Krok aktywacji - USUWANIE STAREJ PAMIĘCI
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((nazwyCache) => {
+            return Promise.all(
+                nazwyCache.map((nazwa) => {
+                    if (nazwa !== CACHE_NAME) {
+                        return caches.delete(nazwa); // Usuwa starsze wersje
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 3. Krok obsługi zapytań
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => {
-                // Jeśli plik jest w pamięci, zwróć go. Jeśli nie, spróbuj pobrać z sieci.
-                return response || fetch(event.request);
-            })
+            .then((response) => response || fetch(event.request))
     );
 });
